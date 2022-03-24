a7e525b938ddc48d547c3b3789e7cb09cc13dd94
Mark Shaw <xsharp@gmail.com>
2022-03-24 16:09:02 +0800

    Update install-v2ray.sh

--- debian/install-v2ray.sh
+++ debian/install-v2ray.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-apt update -y && \
-    apt install -y \
-        curl
-
-curl -Ls https://install.direct/go.sh | bash
-
+bash <(curl -L -s https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
